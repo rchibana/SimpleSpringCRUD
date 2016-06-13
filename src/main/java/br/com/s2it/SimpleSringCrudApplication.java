4a326c79f1de1000f6846921099f168d77292bc9
Rodrigo Chibana <chibana_rodrigo@hotmail.com>
2016-06-12 23:05:05 -0300

    initial commit

---
+++ src/main/java/br/com/s2it/SimpleSringCrudApplication.java
@@ -0,0 +1,12 @@
+package br.com.s2it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSringCrudApplication.class, args);
+	}
+}
